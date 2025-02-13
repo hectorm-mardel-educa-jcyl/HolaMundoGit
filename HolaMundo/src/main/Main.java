092272d5d6094b9b177064b2122238b8dbd8bfd0
hector <hector@domain.com>
2025-02-13 18:32:07 +0100

    Método acortado

--- HolaMundo/src/main/Main.java
+++ HolaMundo/src/main/Main.java
@@ -1,27 +1,10 @@
-/**
- * 
- */
 package main;
 
-/**
- * 
- */
 public class Main {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo.");
-		adios();
+		System.out.println("1 cambio - Consola.");
 		System.out.println("1 cambio - Eclipse");
 		System.out.println("2 cambio - Consola");
 		System.out.println("2 cambio - Eclipse");
 	}
-
-	public static void adios() {
-		System.out.println("Adios Mundo.");
-	}
-
 }
