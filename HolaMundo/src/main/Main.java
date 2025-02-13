b66656f0fdad8c16744ea4dd9bf30faacc40d835
hector <hector@domain.com>
2025-02-13 18:43:39 +0100

    cambio 3 - Consola

--- HolaMundo/src/main/Main.java
+++ HolaMundo/src/main/Main.java
@@ -6,5 +6,6 @@ public class Main {
 		System.out.println("1 cambio - Eclipse");
 		System.out.println("2 cambio - Consola");
 		System.out.println("2 cambio - Eclipse");
+		System.out.println("3 cambio - Consola");
 	}
 }
