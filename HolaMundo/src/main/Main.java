d10ff8ddf3519c6a3e4ede939a3aa11fc8e9b2a2
hector <hector@domain.com>
2025-02-14 11:39:43 +0100

    Cleaned messages

--- HolaMundo/src/main/Main.java
+++ HolaMundo/src/main/Main.java
@@ -2,15 +2,6 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("1 cambio - Consola.");
-		System.out.println("1 cambio - Eclipse");
-		System.out.println("2 cambio - Consola");
-		System.out.println("2 cambio - Eclipse");
-		System.out.println("3 cambio - Eclipse");
-		System.out.println("3 cambio - Consola");
-		System.out.println("Subida a Github");
 		System.out.println("Probando HTTPS PAT");
-		System.out.println("Probando HTTPS PAT 2");
-		System.out.println("Probando HTTPS PAT 3");
 	}
 }
